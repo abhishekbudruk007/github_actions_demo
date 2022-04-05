cc6b016e8a021a85e18b2d4054fea9c9fcf5f3e6
AB075440 <Abhishek.Budruk@Cerner.com>
2022-04-05 15:43:33 +0530

    Whitenoise Lib Added

--- github_actions/settings.py
+++ github_actions/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # os.makedirs(STATICFILES_DIRS, exist_ok=True)
 # os.makedirs(STATIC_ROOT, exist_ok=True)
 
